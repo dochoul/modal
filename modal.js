1738a6e57a3eb4dfa6699e3f82805ee53aa345b0
DESKTOP-DAB6NIR\kimdy <kimdy@gabia.com>
2018-04-03 15:33:02 +0900

    makeDiv function add

--- modal.js
+++ modal.js
@@ -6,22 +6,18 @@
 
   var modal = {
 
-    isAjax: false,
-    modalWrap: 'gt-modal-wrap',
     clonedModal: 'cloned-modal',
 
     open: function(obj) {
       //ajax or ajax not, there is no try...
       switch(obj.target.charAt(0)) {
         case '#':
-          this.isAjax = false;
           this.notAjax(obj);
           break;
         case '.':
           console.error('gtris(v1.2.0): Target accept only ID or URL.');
           break;
         default:
-          this.isAjax = true;
           this.ajax(obj);
           break;
       }
@@ -31,59 +27,55 @@
 
       getAjax(obj.target, function(response) {
 
-        self.modalWrap = document.createElement("div");
-        self.modalWrap.classList.add('gt-modal-wrap');
-        document.body.appendChild(self.modalWrap);
-
+        var modalWrap = self.createDiv('gt-modal-wrap');
         var parser = new DOMParser();
-        var el = parser.parseFromString(response, "text/html");
-        el.querySelector('.gt-modal').style.display = 'block';
-        self.modalWrap.appendChild(el.querySelector('.gt-modal'));
+        var paeseHTML = parser.parseFromString(response, "text/html");
 
-        self.modalWrap.querySelector('[data-modal="hide"]').addEventListener('click', function() {
+        document.body.appendChild( modalWrap );
+        paeseHTML.querySelector('.gt-modal').style.display = 'block';
+        modalWrap.appendChild( paeseHTML.querySelector('.gt-modal') );
+
+        modalWrap.querySelector('[data-modal="hide"]').addEventListener('click', function() {
           self.close(obj);
         });
 
         //completed event return
         if(obj.completed) return obj.completed();
-
       });
     },
     notAjax: function(obj) {
-      var modalWindow;
+      var modalWrap = this.createDiv('gt-modal-wrap');
+      var modalWindow = document.querySelector(obj.target);
 
-      //모달 윈도우를 래핑하는 디비전을 만들고 문서의 가장 하단에 append 한다.
-      this.modalWrap = document.createElement("div");
-      this.modalWrap.classList.add('gt-modal-wrap');
-      document.body.appendChild(this.modalWrap);
-
-      //문서에서 모달 윈도우를 가져온다.
-      modalWindow = document.querySelector(obj.target);
+      //모달 윈도우를 래핑하는 디비전을 문서의 가장 하단에 append 한다.
+      document.body.appendChild(modalWrap);
 
       if(modalWindow) {
-        this.modalWrap.appendChild(modalWindow);
+        modalWrap.appendChild(modalWindow);
         modalWindow.style.display = 'block';
         modalWindow.querySelector('[data-modal="hide"]').addEventListener('click', this.close, false);
         this.clonedModal = modalWindow.cloneNode(true);
       }else{
-        this.modalWrap.appendChild(this.clonedModal);
-        this.clonedModal.style.display = 'block';
+        modalWrap.appendChild(this.clonedModal);
         this.clonedModal.querySelector('[data-modal="hide"]').addEventListener('click', this.close, false);
       }
     },
     close: function(obj) {
-      if(Element.prototype.remove) {
-        modal.modalWrap.remove();
-      }else{
-        modal.modalWrap.parentNode.removeChild(modalWrap);
-      }
+      var modalWrap = document.querySelector('.gt-modal-wrap');
+      document.body.removeChild(modalWrap);
       if(obj.closed) return obj.closed();
+    },
+    createDiv: function(className) {
+      var div = document.createElement('div');
+      div.className = className;
+      return div;
     }
   };
 
   gtris.ui.modal = modal;
 
 })(window.gtris);
+
 
 
 function getAjax(url, success) {
