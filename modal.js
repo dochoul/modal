51a821b6d033f5823c311d2d30472c5cd8b78a86
DESKTOP-DAB6NIR\kimdy <kimdy@gabia.com>
2018-04-10 15:10:14 +0900

    showModal function add

--- modal.js
+++ modal.js
@@ -6,58 +6,59 @@
 
   var modal = {
 
+    isAjax: false,
     clonedModal: 'cloned-modal',
 
     open: function(obj) {
       //ajax or ajax not, there is no try...
       switch(obj.target.charAt(0)) {
         case '#':
+          this.isAjax = false;
           this.notAjax(obj);
           break;
         case '.':
+          this.isAjax = false;
+          this.notAjax(obj);
           console.error('gtris(v1.2.0): Target accept only ID or URL.');
           break;
         default:
+          this.isAjax = true;
           this.ajax(obj);
           break;
       }
     },
     ajax: function(obj) {
       var self = this;
-
       getAjax(obj.target, function(response) {
-
-        var modalWrap = self.createDiv('gt-modal-wrap');
         var parseHTML = new DOMParser().parseFromString(response, 'text/html');
-
-        document.body.appendChild( modalWrap );
-        parseHTML.querySelector('.gt-modal').style.display = 'block';
-        modalWrap.appendChild( parseHTML.querySelector('.gt-modal') );
-
-        modalWrap.querySelector('[data-modal="hide"]').addEventListener('click', function() {
-          self.close(obj);
-        });
-
-        //completed event return
-        if(obj.completed) return obj.completed();
+        self.showModal(parseHTML, obj);
       });
     },
     notAjax: function(obj) {
-      var modalWrap = this.createDiv('gt-modal-wrap');
-      var modalWindow = document.querySelector(obj.target);
-
-      //모달 윈도우를 래핑하는 디비전을 문서의 가장 하단에 append 한다.
-      document.body.appendChild(modalWrap);
-
-      if(modalWindow) {
-        modalWrap.appendChild(modalWindow);
-        modalWindow.style.display = 'block';
-        modalWindow.querySelector('[data-modal="hide"]').addEventListener('click', this.close, false);
+      if(document.querySelector(obj.target)) {
         this.clonedModal = modalWindow.cloneNode(true);
+        this.showModal(modalWindow, obj);
       }else{
-        modalWrap.appendChild(this.clonedModal);
-        this.clonedModal.querySelector('[data-modal="hide"]').addEventListener('click', this.close, false);
+        this.showModal(this.clonedModal, obj);
       }
+    },
+    showModal: function(modalWindow, obj) {
+      var self = this;
+      if(this.isAjax) {
+        modalWindow = modalWindow.querySelector('.gt-modal');
+      }else{
+        modalWindow = modalWindow;
+      }
+      var modalWrap = this.createDiv('gt-modal-wrap');
+      document.body.appendChild( modalWrap );
+      modalWindow.style.display = 'block';
+      modalWrap.appendChild( modalWindow );
+      modalWrap.querySelector('[data-modal="hide"]').addEventListener('click', function() {
+        self.close(obj);
+      });
+
+      //completed event return
+      if(obj.completed) return obj.completed();
     },
     close: function(obj) {
       var modalWrap = document.querySelector('.gt-modal-wrap');
@@ -108,14 +109,19 @@ function DOMParser(text) {
 
 
 function getAjax(url, success) {
-    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-    xhr.open('GET', url);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
-    };
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    xhr.send();
-    return xhr;
+  var xhr;
+  if(window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest(); //for modern browsers
+  }else{
+    xhr = new ActiveXObject("Microsoft.XMLHTTP"); //for old IE browsers
+  }
+  xhr.open('GET', url);
+  xhr.onreadystatechange = function() { //status 200: 성공
+    if (xhr.readyState > 3 && xhr.status === 200) success(xhr.responseText); //반환된 텍스트
+  };
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  xhr.send();
+  return xhr;
 }
 
 function $(selector, context) {
